83436d4c987659c7c5c89af5cca302ea41eeff54
tomag <tomergross88@gmail.com>
2016-07-05 12:21:53 +0300

    Solution for: sol-python-verint.syntax-lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -1,9 +1,28 @@
 """
-Write a program that randomizes 2 numbers
-and calculates their least common multiplier,
-that is the smallest number that is divisable
-by both.
-For example if the numbers were 4 and 6,
-program should print 12
 """
+from random import randint
 
+checkers = [2,3,5,7]
+def getPrimes(num):
+    primes = {} 
+    for checker in checkers:
+        primes[checker] = 0        
+        while (num % checker == 0):
+            primes[checker]+=1
+            num /= checker
+    return primes
+
+firstNum = randint(1,10)
+firstPrimes = getPrimes(firstNum)
+secondNum = randint(1,10)
+secondPrimes = getPrimes(secondNum)
+ 
+print "First: {}. Primes: {}".format(firstNum, firstPrimes)
+print "Second: {}. Primes: {}".format(secondNum, secondPrimes)
+
+multiple = 1
+for checker in checkers:
+    maxTimes = max(firstPrimes[checker], secondPrimes[checker])
+    if (maxTimes != 0):
+        multiple *= maxTimes * checker
+print "Multiple: ", multiple
