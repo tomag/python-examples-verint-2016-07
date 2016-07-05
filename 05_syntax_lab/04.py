fc7c5ff3ecafc05c6620eb5f83f89fa17bc7b6a5
tomag <tomergross88@gmail.com>
2016-07-05 12:21:55 +0300

    Solution for: sol-python-verint.syntax-lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -1,8 +1,34 @@
 """
-Write a program that reads lines from the user
-until an empty line is inserted.
-After the user typed in an empty line,
-print all previously inserted lines in reverse
-order (from last to first)
+    Input: Lines from user, until empty line
+    Output: All lines from user, from the end to beginning
 """
+import sys
 
+inputList = []
+inputIndex = 1
+
+def getInputFromUser(inputIndex):
+    print "Line {}: ".format(inputIndex),
+    input = raw_input()
+    return input
+
+print
+print "Enter lines as you wish. Press Another ENTER to finish"
+input = getInputFromUser(inputIndex)
+
+while (len(input) > 0):
+    inputIndex+=1
+    inputList.insert(0, input)
+    input = getInputFromUser(inputIndex)    
+
+if (len(inputList) == 0):
+    print "you did not enter any input. Exiting...".capitalize()
+    sys.exit()
+    
+print
+print "you entered:".title()
+
+index = 1
+for line in inputList:
+    print "{}. ".format(index), line
+    index+=1
