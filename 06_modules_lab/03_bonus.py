cee641e68229b1d15dd3860cbcc2d9d0e5919e30
tomag <tomergross88@gmail.com>
2016-07-06 12:20:09 +0300

    Solution for: sol-python-verint.modules-lab/03_bonus.py

--- 06_modules_lab/03_bonus.py
+++ 06_modules_lab/03_bonus.py
@@ -1,13 +1,27 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
-
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
-
-- Take threshold and path as command line arguments
-
-Bonus: Use argparse module to parse command line arguments
 """
+The program receives a root directory to search files in, 
+and propmts the user with files larger than a minimum size (optional, default 1024KB) and asks the user to delete those files
+"""
+import os, sys, argparse
 
+def deletionSequence(filePath):
+    print "Do you wish to delete the file? [y/n]",
+    answer = raw_input()
+    if (answer.lower() == "y"):
+        os.remove(filePath)
+
+parser = argparse.ArgumentParser(description= "Runs over files under the given root directory. Prompts deletion message for file larger than minimum size")
+parser.add_argument("rootDir", help="Root directory to start checking files")
+parser.add_argument("--minSizeInKB", "-mskb", help="Minimum file size to propmt deletion message to user",type=int, default=1024)
+
+args = parser.parse_args()
+minSizeInKB = args.minSizeInKB
+rootDir = args.rootDir
+
+for root, dirs, files in os.walk(rootDir):
+    for file in files:
+        filePath = os.path.join(root, file)
+        fileSizeInKB = round(float(os.path.getsize(filePath)) / 1024)
+        if (fileSizeInKB > minSizeInKB):
+            print "{} {} KB".format(filePath, int(fileSizeInKB))
+            deletionSequence(filePath)
