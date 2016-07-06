06a30085bece0fbf29c86b0eb930afa0817a86f9
tomag <tomergross88@gmail.com>
2016-07-06 12:20:11 +0300

    Solution for: sol-python-verint.modules-lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -1,4 +1,16 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
 """
+The program receives two numbers as command line arguments
+And prints their sum.
 
+If the user did not enter exactly 2 command line arguments,
+The usage is printed to the screen. 
+"""
+import sys
+
+if (len(sys.argv) != 3):
+    print "Usage: {} <firstNum> <secondNum>", sys.argv[0]
+    sys.exit()
+
+(prName, firstNum, secondNum) = sys.argv
+
+print int(firstNum) + int(secondNum)
