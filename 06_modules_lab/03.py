a242d65a1157e1f4f7365c3c81d1abf9c33393dd
tomag <tomergross88@gmail.com>
2016-07-06 12:20:08 +0300

    Solution for: sol-python-verint.modules-lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -1,11 +1,25 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
-
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
-
-- Take threshold and path as command line arguments
 """
+The program receives a root directory to search files in, 
+and propmts the user with files larger than a minimum size (optional, default 1024KB) and asks the user to delete those files
+"""
+import os, sys, argparse
 
+def deletionSequence(filePath):
+    print "Do you wish to delete the file? [y/n]",
+    answer = raw_input()
+    if (answer.lower() == "y"):
+        os.remove(filePath)
+
+if (len(sys.argv) != 3)
+    print "Usage: {} <rootDir> <minSizeInKB>", sys.argv[0]
+    sys.exit()
+    
+(prName, rootDir, minSizeInKB) = sys.argv
+
+for root, dirs, files in os.walk(rootDir):
+    for file in files:
+        filePath = os.path.join(root, file)
+        fileSizeInKB = round(float(os.path.getsize(filePath)) / 1024)
+        if (fileSizeInKB > minSizeInKB):
+            print "{} {} KB".format(filePath, int(fileSizeInKB))
+            deletionSequence(filePath)
