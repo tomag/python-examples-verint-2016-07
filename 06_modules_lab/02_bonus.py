237e8519a97d41ad9ebdb6e2a6914894dadbe779
tomag <tomergross88@gmail.com>
2016-07-06 12:20:06 +0300

    Solution for: sol-python-verint.modules-lab/02_bonus.py

--- 06_modules_lab/02_bonus.py
+++ 06_modules_lab/02_bonus.py
@@ -1,10 +1,20 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
-Bonus: Print error messages for invalid inputs.
-
-To print error messages we'll have to use a concept not yet learned in the
-course, and which will only be presented later: Exceptions.
-We'll tap into python's error handling and change its default
-error message to something more meaningful.
 """
+The program receives two numbers as command line arguments
+And prints their sum.
 
+If the user did not enter exactly 2 command line arguments,
+The usage is printed to the screen. 
+"""
+import sys
+
+if (len(sys.argv) != 3):
+    print "Usage: {} <firstNum> <secondNum>", sys.argv[0]
+    sys.exit()
+
+(prName, firstNum, secondNum) = sys.argv
+
+if (firstNum.isdigit() is False or secondNum.isdigit() is False):
+    print "Input must be NUMBERS."
+    sys.exit()
+
+print int(firstNum) + int(secondNum)
