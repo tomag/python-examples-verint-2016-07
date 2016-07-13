9c0da3ff664a76d034db56de275fd59723b1bbff
tomag <tomergross88@gmail.com>
2016-07-13 10:35:07 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,5 +1,28 @@
 """
-Write a program that prints only unique values
-from sys.argv (without program name)
+The program receives input file containing a word in each line.
+Prints all anagrams in the same line
 """
 
+import sys
+from collections import Counter
+
+if (len(sys.argv) != 2):
+    exit("Usage: 02.py <fileName>")
+
+fileName = sys.argv[1]
+
+repeats = {}
+
+with open(fileName, "r") as fin:
+    for line in fin:
+        line = line.rstrip()
+        sortedLine = ''.join(sorted(line))
+        if sortedLine in repeats:
+            repeats[sortedLine].append(line)
+        else:
+            repeats[sortedLine] = [line]
+
+for values in repeats.values():
+    for value in values:
+        print value,
+    print
