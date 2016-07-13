921cb8464d23ca97e219b3e4443dde03f8e145a7
tomag <tomergross88@gmail.com>
2016-07-13 12:28:09 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,11 @@
 """
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
 """
 
+startIndex = 97
+totalLetters = 26
+
+letters = [chr(i) for i in range(startIndex, startIndex + totalLetters)]
+
+allOptions = ["{}{}{}".format(i,j,k) for i in letters for j in letters for k in letters]
+
+print allOptions
