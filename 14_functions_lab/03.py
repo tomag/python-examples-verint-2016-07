e08c949288b7b879437961673625d72948435e97
tomag <tomergross88@gmail.com>
2016-07-14 15:06:53 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -1,5 +1,12 @@
 """
-Write a function that calculates the sum
-of the 10th digit from all arguments passed to it
 """
 
+def sumoftens(*numbers):
+    sum = 0
+    for num in numbers:
+        if num <= 9: continue
+        num /= 10
+        sum += num % 10
+    return sum    
+
+print sumoftens(10,123,3453,234,1,6,555)
