1b7494945d95daa3b7dad5d66e7339536543d20b
tomag <tomergross88@gmail.com>
2016-07-14 15:06:57 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,7 +1,15 @@
 """
-Write a function that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
 """
 
+import sys
+
+def isrighttype(number, text):
+    if number.isdigit() == False:
+        raise Exception("Input '{}' is not a valid number".format(number))
+
+if len(sys.argv) != 3:
+    sys.exit("Usage: 02.py <number> <string>")
+
+(number, text) = sys.argv[1:]
+
+isrighttype(number, text)
