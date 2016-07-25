a257251653badbbaab291246989e4cc125f1b437
tomag <tomergross88@gmail.com>
2016-07-25 13:09:49 +0300

    Solution for: sol-python-verint.class-syntax-lab/03.py

--- 20_classes_lab/03.py
+++ 20_classes_lab/03.py
@@ -11,3 +11,40 @@ depends on.
 Implement widget so the following code works
 """
 
+class Widget(object):
+
+    def __init__(self, name):
+        self._name = name
+        self._dependencies = []
+        self._is_built = False
+
+    def add_dependency(self, *widgets):
+        self._dependencies.extend(widgets)
+
+    def build(self):
+        if (self._is_built): return
+
+        for dep in self._dependencies:
+            dep.build()
+        print self._name
+        
+        self._is_built = True
+
+luke    = Widget("Luke")
+hansolo = Widget("Han Solo")
+leia    = Widget("Leia")
+yoda    = Widget("Yoda")
+padme   = Widget("Padme Amidala")
+anakin  = Widget("Anakin Skywalker")
+obi     = Widget("Obi-Wan")
+darth   = Widget("Darth Vader")
+_all    = Widget("All")
+
+
+luke.add_dependency(hansolo, leia, yoda)
+leia.add_dependency(padme, anakin)
+obi.add_dependency(yoda)
+darth.add_dependency(anakin)
+
+_all.add_dependency(luke, hansolo, leia, yoda, padme, anakin, obi, darth)
+_all.build()
